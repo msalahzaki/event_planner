5b6c63ce6cef12fec3eabc0811aa127cd79223ae
M_Salah <m.salah.zaki@gmail.com>
2024-12-17 17:34:18 +0200

    Project Init

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,26 @@
+class AppAssets {
+  static String Logo = "assets/images/Logo.png";
+  static String SplashLogo = "assets/images/SplashLogo.png";
+  static String Splash_Tail = "assets/images/Splash_Tail.png";
+  static String Intro_1 = "assets/images/Intro_1.png";
+  static String Intro_2 = "assets/images/Intro_2.png";
+  static String Intro_3 = "assets/images/Intro_3.png";
+  static String Intro_4 = "assets/images/Intro_4.png";
+  static String Reset_Pass = "assets/images/Reset_Pass.png";
+  static String Route = "assets/images/Route.png";
+  static String Birthday = "assets/images/Birthday.png";
+  static String Book_Club = "assets/images/Book_Club.png";
+  static String Eating = "assets/images/Eating.png";
+  static String Exhibition = "assets/images/Exhibition.png";
+  static String Gaming = "assets/images/Gaming.png";
+  static String Holiday = "assets/images/Holiday.png";
+  static String Sport = "assets/images/Sport.png";
+  static String Meeting = "assets/images/Meeting.png";
+  static String Workshop = "assets/images/Workshop.png";
+  static String Google_icon = "assets/icons/Google_icon.png";
+  static String Egypt_icon = "assets/icons/Egypt_icon.png";
+  static String USA_icon = "assets/icons/USA_icon.png";
+
+
+
+}
---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primarycolor = Color(0xff5669FF);
+  static const Color black = Color(0xff1C1C1C);
+
+
+  static const Color white = Color(0xffffffff);
+  static const Color grey = Color(0xff7B7B7B);
+  static const Color transpernt = Colors.transparent;
+  static const Color gold = Color(0xffFEFFE8);
+}
